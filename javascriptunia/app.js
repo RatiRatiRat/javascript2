445df6306fb72c4abe20ccf8942312e069ddffbb
RatiRatiRat <150704351+RatiRatiRat@users.noreply.github.com>
2023-12-11 18:31:57 +0300

    Add files via upload

---
+++ javascriptunia/app.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('button')
+
+
+
+
+
+
+button.addEventListener('click', () => {
+    const img = document.querySelector('.nadziralaaaa'); // Selects the first image element with the class 'nadziralaaaa'
+    img.classList.toggle('active'); // Toggles the 'active' class on the selected image
+});
